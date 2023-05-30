fc6ad491a2a9a06de506ab4248ddddfdbea6ca55
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-05-30 14:45:53 +0200

    Request2

---
+++ app1/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('apfelbaum/', views.apfelbaum, name="apfelbaum"),
+    path('apfelbaum/<str:name>/<int:number>', views.apfelbaum_n, name="apfelbaum"),
+    path('mutter/<int:id>', views.mutter, name="mutter"),
+]
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Jetzt geht es aber")
+
+def apfelbaum(request):
+    return HttpResponse("Ja, das ist ein Apfelbaum")
+
+def apfelbaum_n(request, name, number):
+    antwort = f"Gute Tag {name}. Sie haben die Zahl {number} übergeben."
+    return HttpResponse(antwort)
+
+def mutter(request, id):
+    ds = Mutter.objects.filter(id=id)
+    if len(ds)==1:
+        #ds = Mutter.objects.get(id=id)
+        return HttpResponse(ds[0])
+    else:
+        return HttpResponse("Ach komm, die Mutter gib es garnicht")
+
+    
